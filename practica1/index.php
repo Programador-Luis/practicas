2be133b6ea37d55598e847027c4582df8f583cb9
Luis González <82612943+Programador-Luis@users.noreply.github.com>
2024-09-18 13:46:50 -0400

    Practica POO

---
+++ practica1/index.php
@@ -0,0 +1,44 @@
+<?php
+
+class Persona{
+    private $nombre;
+    private $apellidos;
+    private $edad;
+
+    function __construct($nombre, $apellidos, $edad){
+        $this->nombre = $nombre;
+        $this->apellidos = $apellidos;
+        $this->edad = $edad;
+    }
+
+
+    function __get($propiedad){
+        if(property_exists($this, $propiedad)) {
+            return $this->$propiedad;
+        }
+    }
+
+    function __set($propiedad, $valor) {
+        if(property_exists($this, $propiedad)) {
+            $this->$propiedad = $valor;
+        }
+    }
+
+    function mayorEdad() {
+        return $this->edad >= 18;
+    }
+
+    function nombreCompleto() {
+        return $this->nombre . " " . $this->apellidos;
+    }
+}
+
+$persona = new Persona("Luis", "González", 22);
+
+if($persona->mayorEdad()) {
+    echo $persona->nombreCompleto() . " es mayor de edad";
+} else {
+    echo $persona->nombreCompleto() . " no es mayor de edad";
+}
+
+?>
